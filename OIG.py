06bc214f8d4a3fb3afed2c78ecae6401c9e646fa
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2023-03-23 22:53:32 +0000

    check files

---
+++ OIG.py
@@ -0,0 +1,105 @@
+"""OIG: """
+
+
+import json
+
+import datasets
+from datasets.tasks import QuestionAnsweringExtractive
+
+
+logger = datasets.logging.get_logger(__name__)
+
+
+_CITATION = """\
+
+"""
+
+_DESCRIPTION = """\
+Open Instruction Generalist (OIG) Dataset is intended to train assistants that are part of the LAION-AI's OpenChat family of assistants.  OIG Assistants will be trained on the OIG dataset, a massive synthetic instructions with the goal of performing many diverse types of tasks.
+"""
+
+_URL = "https://"
+_URLS = {
+    "train": [],
+    "dev": [],
+}
+
+
+class OIGConfig(datasets.BuilderConfig):
+    """BuilderConfig for OIG."""
+    ...
+
+class OIG(datasets.GeneratorBasedBuilder):
+    """OIG: The Open Instruction Generalist Dataset"""
+
+    BUILDER_CONFIGS = [
+        OIGConfig(
+            name="plain_text",
+            version=datasets.Version("0.0.0", ""),
+            description="Plain text",
+        ),
+    ]
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "title": datasets.Value("string"),
+                    "context": datasets.Value("string"),
+                    "question": datasets.Value("string"),
+                    "answers": datasets.features.Sequence(
+                        {
+                            "text": datasets.Value("string"),
+                            "answer_start": datasets.Value("int32"),
+                        }
+                    ),
+                }
+            ),
+            # No default supervised_keys (as we have to pass both question
+            # and context as input).
+            supervised_keys=None,
+            homepage="https://rajpurkar.github.io/SQuAD-explorer/",
+            citation=_CITATION,
+            task_templates=[
+                QuestionAnsweringExtractive(
+                    question_column="question", context_column="context", answers_column="answers"
+                )
+            ],
+        )
+
+    def _split_generators(self, dl_manager):
+        downloaded_files = dl_manager.download_and_extract(_URLS)
+
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["dev"]}),
+        ]
+
+    def _generate_examples(self, filepath):
+        """This function returns the examples in the raw (text) form."""
+        logger.info("generating examples from = %s", filepath)
+        key = 0
+        with open(filepath, encoding="utf-8") as f:
+            squad = json.load(f)
+            for article in squad["data"]:
+                title = article.get("title", "")
+                for paragraph in article["paragraphs"]:
+                    context = paragraph["context"]  # do not strip leading blank spaces GH-2585
+                    for qa in paragraph["qas"]:
+                        answer_starts = [answer["answer_start"] for answer in qa["answers"]]
+                        answers = [answer["text"] for answer in qa["answers"]]
+                        # Features currently used are "context", "question", and "answers".
+                        # Others are extracted here for the ease of future expansions.
+                        yield key, {
+                            "title": title,
+                            "context": context,
+                            "question": qa["question"],
+                            "id": qa["id"],
+                            "answers": {
+                                "answer_start": answer_starts,
+                                "text": answers,
+                            },
+                        }
+                        key += 1
